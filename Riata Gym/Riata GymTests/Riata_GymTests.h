7fadb2377da5154e82049e3a7bd33c663880ac7f
Patrick Hagen <phagen@wisc.edu>
2012-12-08 14:35:35 -0600

    Blank Project Commit

---
+++ Riata Gym/Riata GymTests/Riata_GymTests.h
@@ -0,0 +1,13 @@
+//
+//  Riata_GymTests.h
+//  Riata GymTests
+//
+//  Created by Patrick Hagen on 12/8/12.
+//  Copyright (c) 2012 Patrick Hagen. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Riata_GymTests : SenTestCase
+
+@end
