a91ac218e86ac1d10f6dcf345e7cbb76d2a325ab
Patrick Hagen <phagen@wisc.edu>
2012-12-08 14:34:12 -0600

    Initial Commit

---
+++ Riata Gym/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Riata Gym
+//
+//  Created by Patrick Hagen on 12/8/12.
+//  Copyright (c) 2012 Patrick Hagen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Riata GymTests/Riata_GymTests.m
@@ -0,0 +1,32 @@
+//
+//  Riata_GymTests.m
+//  Riata GymTests
+//
+//  Created by Patrick Hagen on 12/8/12.
+//  Copyright (c) 2012 Patrick Hagen. All rights reserved.
+//
+
+#import "Riata_GymTests.h"
+
+@implementation Riata_GymTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Riata GymTests");
+}
+
+@end
