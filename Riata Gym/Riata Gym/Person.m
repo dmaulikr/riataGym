e4fe5def0a36817665fb2def7e6aef991124c057
Patrick Hagen <phagen@wisc.edu>
2012-12-09 12:27:11 -0600

    Determined how to use relationships in Core Data. Sample code in App Delegate

--- Riata Gym/Riata Gym/Person.m
+++ Riata Gym/Riata Gym/Person.m
@@ -7,11 +7,25 @@
 //
 
 #import "Person.h"
+#import "Workout.h" 
 
 
 @implementation Person
 
 @dynamic name;
 @dynamic workout;
+
+- (void)addWorkoutObject:(NSManagedObject *)value
+{
+    self.workout = [self.workout setByAddingObject:value];
+    //Workout *test = nil;
+    NSArray *array = nil;
+    array = [self.workout allObjects];
+    NSLog(@"%@",((Workout*)[array objectAtIndex:0]));
+}
+- (void)addWorkout:(NSSet *)values
+{
+    
+}
 
 @end
